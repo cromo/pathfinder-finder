b60ddb14eda70f445f5e2b389201f918cbd47367
Cristian Romo <cristian.g.romo@gmail.com>
2018-02-13 21:46:02 -0600

    Add ability to scrub through anti-arborescence building algorithm steps

--- sketch.js
+++ sketch.js
@@ -6,6 +6,9 @@ let nodes = [];
 let oneWayWalls = [];  // Represents cliffs or similar geometry
 let goalNode = 0;
 
+let stepSelector = null;
+let firstDrawAfterStateChange = true;
+
 function completeDirectedGraph(nodes, edges) {
   nodes.forEach((_, i) => {
     nodes.forEach((_, j) => {
@@ -44,11 +47,13 @@ function setup() {
   canvas.mouseClicked(() => {
     if (toolSelector.value() == 'nodes') {
       addNode(mouseX, mouseY);
+      firstDrawAfterStateChange = true;
       redraw();
       saveState(goalNode, nodes, oneWayWalls);
     } else if (toolSelector.value() == 'walls') {
       const wallAdded = addWallPoint(mouseX, mouseY);
       if (wallAdded) {
+        firstDrawAfterStateChange = true;
         redraw();
         saveState(goalNode, nodes, oneWayWalls);
       }
@@ -59,9 +64,14 @@ function setup() {
     if (this.value() && 0 <= value && value < nodes.length) {
       goalNode = value;
     }
+    firstDrawAfterStateChange = true;
     redraw();
     saveState(goalNode, nodes, oneWayWalls);
   });
+
+  stepSelector = createSlider(0, 0, 0, 1);
+  stepSelector.input(redraw);
+
   window.onpopstate = event => loadState(event.state);
 
   initialState = new URL(window.location).searchParams.get("s");
@@ -72,25 +82,33 @@ function setup() {
   noLoop();
 }
 
-function drawArrow(x0, y0, x1, y1) {
+function drawArrow(x0, y0, x1, y1, color) {
+  color = color || 0;
   const direction = createVector(x0, y0).sub(createVector(x1, y1)).heading();
+  stroke(color);
   line(x0, y0, x1, y1);
   push();
   translate(x1, y1);
   rotate(direction + PI);
-  fill(0);
+  fill(color);
   // This points right because 0 radians lies along the x axis.
   triangle(0, 0, -10, 6, -10, -6);
   pop();
 }
 
-function drawEdge(edge) {
+function drawEdge(edge, color) {
   const startNode = nodes[edge[0]];
   const endNode = nodes[edge[1]];
   const difference = p5.Vector.sub(endNode, startNode);
   const magnitude = difference.mag();
   const toEdge = difference.copy().setMag(magnitude - NODE_RADIUS / 2);
-  drawArrow(startNode.x, startNode.y, startNode.x + toEdge.x, startNode.y + toEdge.y);
+  drawArrow(startNode.x, startNode.y, startNode.x + toEdge.x, startNode.y + toEdge.y, color);
+}
+
+function drawNode(x, y, strokeColor, fillColor) {
+  stroke(strokeColor || 0);
+  fill(fillColor || 0);
+  ellipse(x, y, NODE_RADIUS, NODE_RADIUS);
 }
 
 function lineSegmentsIntersect(p1, p2, p3, p4) {
@@ -141,9 +159,10 @@ function isTraversible(edge) {
   return !edgeHitsAnyOneWayWalls(edge);
 }
 
-function drawLineWithNormal(start, end) {
+function drawLineWithNormal(start, end, color) {
   const center = p5.Vector.add(start, end).div(2);
   const normal = p5.Vector.sub(end, start).div(8).rotate(HALF_PI);
+  stroke(color || 0);
   line(start.x, start.y, end.x, end.y);
   line(center.x, center.y, center.x + normal.x, center.y + normal.y);
 }
@@ -169,12 +188,22 @@ function removeEdgesLeavingSubgraph(nodes, edges) {
 }
 
 function pruneToTree(goalNodeIndex, edges) {
+  const iterationStates = [];
+  function addIterationState(currentAntiarborescentEdges, candidateEdges, addedEdges) {
+    // TODO? add weights to this state so that they can be visualized as well
+    iterationStates.push({
+      current: currentAntiarborescentEdges.slice(),
+      candidates: candidateEdges.slice(),
+      new: addedEdges.slice(),
+    });
+  }
+
   let branches = [];
   let reachedNodes = [goalNodeIndex];
-  let nodeDistancesToGoal = {};
+  let nodeDistancesToGoal = new Map();
   nodeDistancesToGoal[goalNodeIndex] = 0;
-
   let previousNodeReachCount = reachedNodes.length;
+
   while (reachedNodes.length < nodes.length) {
     // Get the edges from the rest of the graph that lead to nodes that have been reached.
     // These edges originate from the nodes we want to add to the reached nodes this iteration.
@@ -197,6 +226,7 @@ function pruneToTree(goalNodeIndex, edges) {
     const closestEdges = Object.values(enteringEdgesByStartNode).map(edgeList => {
       return edgeList[0];
     });
+    addIterationState(branches, edgesEnteringSubgraph, closestEdges[0] ? [closestEdges[0]] : []);
     // Then add the first edge to our tree, and add the start node to our subgraph.
     if (closestEdges.length) {
       const newEdge = closestEdges[0];
@@ -208,25 +238,39 @@ function pruneToTree(goalNodeIndex, edges) {
     }
     previousNodeReachCount = reachedNodes.length;
   }
-  return branches;
+  addIterationState(branches, [], []);
+  return iterationStates;
 }
-var reachabilityEdges;
+
 function draw() {
   background(240);
 
   const edges = [];
   completeDirectedGraph(nodes, edges);
-  reachabilityEdges = edges.filter(isTraversible);
-  pruneToTree(goalNode, reachabilityEdges).forEach(drawEdge);
+  const reachabilityEdges = edges.filter(isTraversible);
+  const algorithmIterations = pruneToTree(goalNode, reachabilityEdges);
+  stepSelector.elt.max = algorithmIterations.length - 1;
+  if (firstDrawAfterStateChange) {
+    stepSelector.value(algorithmIterations.length - 1);
+  }
 
-  console.log("nodes:", nodes);
-  nodes.forEach((node, i) => {
-    fill(goalNode == i ? 0 : 255);
-    ellipse(node.x, node.y, NODE_RADIUS, NODE_RADIUS);
-  });
+  function drawIteration({current, candidates, new: added}) {
+    current.forEach(drawEdge);
+    candidates.forEach(e => drawEdge(e, color(0, 100, 200)));
+    added.forEach(e => drawEdge(e, color(0, 170, 0)));
+
+    nodes.forEach((node, i) => {
+      drawNode(node.x, node.y, 0, goalNode == i ? 0 : 255);
+    });
+  }
+
+  // drawIteration(algorithmIterations[algorithmIterations.length - 1]);
+  drawIteration(algorithmIterations[stepSelector.value()]);
   oneWayWalls.forEach(([start, end]) => {
     drawLineWithNormal(start, end);
   });
+
+  firstDrawAfterStateChange = false;
 }
 
 // Save enough information to recover the current state in the URL.
